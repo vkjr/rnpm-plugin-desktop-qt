52d442b0ae50e77cbdde789eeb75973de1b2daf8
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-02 13:07:22 +0300

    Fixed errors

--- src/desktop-qt.js
+++ src/desktop-qt.js
@@ -2,13 +2,13 @@
 
 const chalk = require('chalk');
 const Common = require('./common');
-
+const execSync = require('child_process').execSync;
 
 module.exports = async function (config, args, options) {
 
   let rndPackage = "git+https://github.com/vkjr/react-native-desktop-qt.git";
 
-  console.log(`Installing ${rnwPackage}...`);
+  console.log(`Installing ${rndPackage}...`);
   const pkgmgr = Common.isGlobalCliUsingYarn(process.cwd()) ? 'yarn add' : 'npm install --save';
   const execOptions = options.verbose ? { stdio: 'inherit' } : {};
   execSync(`${pkgmgr} ${rndPackage}`, execOptions);
