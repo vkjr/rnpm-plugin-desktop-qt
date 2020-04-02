eaeeb20dfc1d601ec181ad237e777eee9815341a
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-02 14:50:30 +0300

    react-native desktop now invokes local-cli/generate-desktop.js after installing react-native-desktop-qt

--- src/common.js
+++ src/common.js
@@ -4,17 +4,28 @@
 const fs = require('fs');
 const path = require('path');
 
-/**
- * Check that 'react-native init' itself used yarn to install React Native.
- * When using an old global react-native-cli@1.0.0 (or older), we don't want
- * to install React Native with npm, and React + Jest with yarn.
- * Let's be safe and not mix yarn and npm in a single project.
- * @param projectDir e.g. /Users/martin/AwesomeApp
- */
+
 const isGlobalCliUsingYarn = function(projectDir) {
   return fs.existsSync(path.join(projectDir, 'yarn.lock'));
 };
 
+
+const getReactNativeAppName = function () {
+  console.log('Reading application name from package.json...');
+  let name = JSON.parse(fs.readFileSync('package.json', 'utf8')).name;
+  if (!name) {
+    if (fs.existsSync('app.json')) {
+      console.log('Reading application name from app.json...');
+      name = JSON.parse(fs.readFileSync('app.json', 'utf8')).name;
+    }
+  }
+  if (!name) {
+    console.error('Please specify name in package.json or app.json');
+  }
+  return name;
+};
+
 module.exports = {
   isGlobalCliUsingYarn,
+  getReactNativeAppName,
 };
--- src/desktop-qt.js
+++ src/desktop-qt.js
@@ -3,14 +3,45 @@
 const chalk = require('chalk');
 const Common = require('./common');
 const execSync = require('child_process').execSync;
+const path = require('path');
 
-module.exports = async function (config, args, options) {
 
+const REACT_NATIVE_DESKTOP_GENERATE_PATH = function() {
+  return path.resolve(
+    process.cwd(),
+    'node_modules',
+    'react-native-desktop-qt',
+    'local-cli',
+    'generate-desktop.js'
+  );
+};
+
+
+function installDesktopPackage(options) {
   let rndPackage = "git+https://github.com/vkjr/react-native-desktop-qt.git";
 
   console.log(`Installing ${rndPackage}...`);
   const pkgmgr = Common.isGlobalCliUsingYarn(process.cwd()) ? 'yarn add' : 'npm install --save';
-  const execOptions = options.verbose ? { stdio: 'inherit' } : {};
+  const execOptions = options.verbose ? {stdio: 'inherit'} : {};
   execSync(`${pkgmgr} ${rndPackage}`, execOptions);
   console.log(chalk.green(`${rndPackage} successfully installed.`));
+}
+
+function runDesktopFilesGenerationScript() {
+
+  const generateDesktop = require(REACT_NATIVE_DESKTOP_GENERATE_PATH());
+  generateDesktop(process.cwd(), Common.getReactNativeAppName());
+}
+
+module.exports = async function (config, args, options) {
+
+  try {
+    installDesktopPackage(options);
+
+    runDesktopFilesGenerationScript();
+  } catch (error) {
+    console.error(chalk.red(error.message));
+    console.error(error);
+    process.exit(1);
+  }
 };
